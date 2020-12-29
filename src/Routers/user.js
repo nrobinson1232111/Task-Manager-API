cbfc828a65a2ad0180e9ed4ff1af4ef6b4599292
Nathan Robinson <nrobinson1232111@gmail.com>
2020-12-29 00:37:17 -0500

    fixed uppercase problems with code imports

--- src/Routers/user.js
+++ src/Routers/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const User = require('../models/User')
+const User = require('../models/user')
 const auth = require('../middlewear/auth')
 const multer = require('multer')
 const sharp = require('sharp')
